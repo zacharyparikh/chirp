a718bca7e40654cb065e5748734ba07d85b7d120
Zachary Parikh <zachary@macbook.local>
2021-06-26 18:51:51 -0400

    Added components and styles directories. Added Chirp logo.

---
+++ client/src/components/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../chirp-logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        Welcome to Chirp.
+          
+      </header>
+    </div>
+  );
+}
+
+export default App;
