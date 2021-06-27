36ae28f285154bfc1e542f2f3cca76b78e4713fd
ishansen <ishans1999@gmail.com>
2021-06-27 19:20:17 +0200

    mongo testing

--- server/dist/app.js
+++ server/dist/app.js
@@ -4,11 +4,58 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = express_1.default();
-const PORT = 8000;
+const PORT = 4000;
+app.use(cors_1.default());
 app.get('/', (req, res) => {
     res.send("Hello, World! Now with changes!");
 });
+// connect to db
+mongoose_1.default.connect("mongodb://127.0.0.1:27017/accounts", {
+    useNewUrlParser: true
+});
+const db = mongoose_1.default.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once("open", function () {
+    console.log("Connection with MongoDB was successful");
+});
+const userSchema = new mongoose_1.default.Schema({
+    name: String,
+    age: Number,
+    password: String,
+    email: { type: String, unique: true },
+}, { timestamps: { createdAt: 'created_at' } });
+userSchema.set('upsert', true);
+userSchema.methods.greetUser = function () {
+    const greeting = "Greetings " + this.name + "!";
+    console.log(greeting);
+};
+const userModel = mongoose_1.default.model('User', userSchema);
+const alice = new userModel({
+    name: 'Alice',
+    age: 30,
+    password: 'password',
+    email: 'alice@wonderland.com'
+});
+alice.greetUser();
+console.log(alice.name);
+alice.greetUser();
+alice.save();
+// run().catch(err => console.log(err));
+// async function run(): Promise<void> {
+//   const alice = new userModel({
+//     name: 'Alice', 
+//     age: 30, 
+//     password: 'password', 
+//     email: 'alice@wonderland.com'
+//   });
+//   alice.greetUser();
+//   await alice.save();
+//   alice.greetUser();
+//   console.log(alice.name);
+// }
 app.listen(PORT, () => {
     console.log(`App Listening at http://localhost:${PORT}`);
 });
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,90 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
-const PORT = 8000;
+const PORT = 4000;
+
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send("Hello, World! Now with changes!");
 });
+
+// connect to db
+mongoose.connect("mongodb://127.0.0.1:27017/accounts", {
+  useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once("open", function() {
+  console.log("Connection with MongoDB was successful");
+}); 
+
+interface User {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  greetUser(): void;
+}
+
+const userSchema = new mongoose.Schema<User>({
+  name: String,
+  age: Number,
+  password: String,
+  email: {type: String, unique: true},
+},
+{ timestamps: { createdAt: 'created_at' } }
+);
+
+userSchema.set('upsert', true);
+
+userSchema.methods.greetUser = function (this: User) {
+  const greeting = "Greetings " + this.name + "!";
+  console.log(greeting);
+}
+
+const userModel = mongoose.model<User>('User', userSchema);
+
+const alice = new userModel({
+  name: 'Alice', 
+  age: 30, 
+  password: 'password', 
+  email: 'alice@wonderland.com'
+});
+alice.greetUser();
+console.log(alice.name);
+alice.greetUser();
+alice.save()
+
+ 
+
+// run().catch(err => console.log(err));
+
+// async function run(): Promise<void> {
+
+//   const alice = new userModel({
+//     name: 'Alice', 
+//     age: 30, 
+//     password: 'password', 
+//     email: 'alice@wonderland.com'
+//   });
+ 
+//   alice.greetUser();
+
+//   await alice.save();
+
+//   alice.greetUser();
+
+//   console.log(alice.name);
+// }
+
+
 
 app.listen(PORT, () => {
   console.log(`App Listening at http://localhost:${PORT}`);
