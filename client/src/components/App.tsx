1c3f196d6cb472861d2362da85206d3d4f86a5e6
Zachary Parikh <parikhzachary@gmail.com>
2021-07-02 18:32:19 -0400

    Added Basic Sign Up Page UI

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
-import logo from '../chirp-logo.svg';
-import '../styles/App.css';
+import { SignUp } from './SignUp';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        Welcome to Chirp.
-          
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/user'>Users</Link>
+          </li>
+
+          <li>
+            <Link to='/signup'>Sign Up</Link>
+          </li>
+
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Feed />
+        </Route>
+      </Switch>
+    </Router>
   );
+}
+
+function Feed() {
+  return <h2>Feed</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
 }
 
 export default App;
