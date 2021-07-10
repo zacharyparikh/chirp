8df20ea09fcd5a31e9e2e0f3373cb3b8a9cd702d
Zachary Parikh <parikhzachary@gmail.com>
2021-07-10 12:23:25 -0400

    Added Home and Login page.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
 import { SignUp } from './SignUp';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Home } from './Home';
+import { Login } from './Login';
 
 function App() {
   return (
@@ -13,40 +9,32 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to='/user'>Users</Link>
+            <Link to="/user">Users</Link>
           </li>
 
           <li>
-            <Link to='/signup'>Sign Up</Link>
+            <Link to="/signup">Sign Up</Link>
           </li>
 
           <li>
-            <Link to='/login'>Login</Link>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       </nav>
 
       <Switch>
         <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Feed />
+          <SignUp />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </Router>
   );
-}
-
-function Feed() {
-  return <h2>Feed</h2>;
-}
-
-function Login() {
-  return <h2>Login</h2>;
 }
 
 export default App;
---
+++ client/src/components/Login.tsx
@@ -0,0 +1,76 @@
+import { Formik, Form, Field } from 'formik';
+import {
+  TextField,
+  Button,
+  Grid,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  button: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export const Login = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.paper}>
+      <Typography component="h1" variant="h5">
+        Login to Chirp
+      </Typography>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        <Form>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Field
+                name="email"
+                label="Email Address"
+                fullWidth
+                variant="outlined"
+                as={TextField}
+              ></Field>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Field
+                name="password"
+                label="Password"
+                fullWidth
+                variant="outlined"
+                as={TextField}
+              ></Field>
+            </Grid>
+          </Grid>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            Login
+          </Button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
